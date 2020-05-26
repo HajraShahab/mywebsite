edd59bce032fb3da318116adf37fe1b1d5194f92
Shayan Ali <shayan.ali.work@gmail.com>
2020-05-26 14:38:13 +0000

    minor fixes

--- README.md
+++ README.md
@@ -14,3 +14,13 @@
 1.  **Open the source code and start editing!**
 
       `src/pages/index.js` contains the main page
+
+1.  **Resources**
+
+    https://material-ui.com/getting-started/usage
+
+    For styling use style={{ color: 'red' }} tag // this is actual css
+    for css https://www.w3schools.com/
+
+    for Typography use props (properties) `variant` as specefied in material ui docs
+    
--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,6 @@ export default () => {
   return (
     // this is where all the code of the main page goes
     <ThemeProvider theme={createMuiTheme(myTheme)}>
-      <Container maxWidth="md">
       <AppBar position="static">
         <Toolbar variant='dense' style={{backgroundColor:"grey"}}>
             <Typography style={{marginRight:'20px'}} variant="h4">About</Typography>
@@ -35,14 +34,15 @@ export default () => {
             <Typography style={{marginRight:'20px'}} variant="h4">Contact</Typography>
         </Toolbar>
       </AppBar> 
-        <Box style={{ marginTop: "100px"}} display="flex" flexDirection="column" alignItems="center" m={4}>
+      <Container maxWidth="md">
+        <Box style={{ marginTop: "100px", display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '32px'}}>
           <Avatar
             style={{ width: "373px", height: "400px" }}
             src="https://media-exp1.licdn.com/dms/image/C4D03AQFwih7E9JQp7Q/profile-displayphoto-shrink_200_200/0?e=1591833600&v=beta&t=P6HnpeP-xdweIDs-MLJjZ-sIjnY-47ZPddAZbWsQIU0"
             />
-          <Typography fontFamily='Roboto' fontSize='52px' style={{ marginTop: "20px"}}variant="h4">Hajra Shahab</Typography>
+          <Typography fontFamily='Roboto' fontSize='52px' style={{ marginTop: "20px"}} variant="h4">Hajra Shahab</Typography>
         </Box>
-          <Box style={{ widht: "400px", height: "255px"}} border={1} bgcolor="grey.500" borderColor="grey.500" borderRadius={16} mt={3} alignItems="center" textAlign="center" fontWeight="fontWeightLight" fontFamily="Roboto" lineHeight="normal">  
+          <Box style={{ widht: "400px", height: "255px", border: '1px solid lightgrey', borderRadius: '16px', marginTop: '24px' , alignItems: "center", textAlign: "center", lineHeight: 'normal'}}  fontWeight="fontWeightLight" fontFamily="Roboto">  
             <Typography variant="body">Hi, I’m Hajra Shahab, a multidisciplinary researcher with profound interest in data analytics, urban informatics policy, and design. I am a strong advocate of interdiscplinary collaborative approach to unlock unconventional solutions to some of the most pressing urban issues that cities face in the 21st century.</Typography>
             </Box>
         <Box>    
