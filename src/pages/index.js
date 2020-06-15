6f9806cd56863e91a00d69e3fd38f812b1e7d283
Hajra Shahab <hajrashahab05@gmail.com>
2020-06-15 23:09:27 +0500

    Background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,15 +25,20 @@ export default () => {
   return (
     // this is where all the code of the main page goes
     <ThemeProvider theme={createMuiTheme(myTheme)}>
+        <Box>
+          <Avatar variant="square"
+          style={{position: 'fixed', bottom: 'fixed', right: 'fixed', width: "1500px", height: "700px", zIndex: "-1"}}
+          img src="https://images.unsplash.com/photo-1581874866735-f3074408881d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=623&q=80" alt="Shibuya Crossing"/>
+        </Box>
       <AppBar position="static">
         <Toolbar variant='dense' style={{backgroundColor:"#fafafa"}}>
             <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginRight:'20px', color: "#CBA01C"}} variant="h5">EN</Typography>
             <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginRight:'auto', color: "#CBA01C"}} variant="h5">JP</Typography> 
-            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginRight:'20px', color: "#CBA01C"}} variant="h5">About</Typography> 
-            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginRight:'20px', color: "#CBA01C"}} variant="h5">Projects</Typography>
-            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginRight:'20px', color: "#CBA01C"}} variant="h5">Blog</Typography>
-            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginRight:'20px', color: "#CBA01C"}} variant="h5">Vlog</Typography>
-            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginRight:'20px', color: "#CBA01C"}} variant="h5">Contact</Typography>
+            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginLeft:'20px', color: "#CBA01C"}} variant="h5">About</Typography> 
+            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginLeft:'20px', color: "#CBA01C"}} variant="h5">Projects</Typography>
+            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginLeft:'20px', color: "#CBA01C"}} variant="h5">Blog</Typography>
+            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginLeft:'20px', color: "#CBA01C"}} variant="h5">Vlog</Typography>
+            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', marginLeft:'20px', color: "#CBA01C"}} variant="h5">Contact</Typography>
         </Toolbar>
       </AppBar> 
       <Container maxWidth="sm">
@@ -42,14 +47,14 @@ export default () => {
             style={{ width: "200px", height: "200px" }}
             src="https://media-exp1.licdn.com/dms/image/C4D03AQFwih7E9JQp7Q/profile-displayphoto-shrink_200_200/0?e=1591833600&v=beta&t=P6HnpeP-xdweIDs-MLJjZ-sIjnY-47ZPddAZbWsQIU0"
             />
-          <Typography style={{href: "https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap", rel: "stylesheet", fontFamily: "'Roboto', sansSerif", fontSize:"52px", marginTop:"20px"}} variant="h4">Hajra Shahab</Typography>
+          <Typography style={{fontFamily: 'Roboto', fontWeight: 'fontWeightLight' , fontSize:"50px", marginTop:"20px"}} variant="h4">Hajra Shahab</Typography>
         </Box>
-          <Box style={{href: "https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap", rel: "stylesheet", fontFamily: "'Roboto', sansSerif", maxWidth: "700px", height: "100px", border: '2px solid lightgrey', marginTop: '40px', alignItems: 'center', textAlign: 'center'}}>  
+          <Box style={{fontFamily: 'Roboto', fontWeight: 'fontWeightLight', maxWidth: "700px", height: "100px", border: '2px solid lightgrey', marginTop: '40px', alignItems: 'center', textAlign: 'center'}}>  
             <Typography variant="body">Hi, I’m Hajra Shahab, a multidisciplinary researcher with profound interest in data analytics, urban informatics policy, and design. I am a strong advocate of interdiscplinary collaborative approach to unlock unconventional solutions to some of the most pressing urban issues that cities face in the 21st century.</Typography>
             </Box>
         <Box>    
-            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', fontWeight: "400px", marginRight:'20px', color: "#CBA01C", textAlign: 'center', marginTop: '10px'}}>Data Analytics   | Public Policy   | Cities  | Transportation and Mobility</Typography>
-            </Box> 
+            <Typography style={{href: "https://fonts.googleapis.com/css2?family=Lora&display=swap", rel: "stylesheet", fontFamily: 'Lora', fontWeight: "400px", marginRight:'20px', color: "#CBA01C", textAlign: 'center', marginTop: '10px'}} variant="h6">Data Analytics   | Public Policy   | Cities  | Transportation and Mobility</Typography>
+            </Box>
       </Container>
     </ThemeProvider>
   )
