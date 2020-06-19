ee9680c90535ef51fa459f3b24a91fad585c69d0
shayanalis <shayan.ali.work@gmail.com>
2020-06-19 23:00:07 +0500

    ui fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,21 +26,19 @@ export default () => {
   return (
     // this is where all the code of the main page goes
     <ThemeProvider theme={createMuiTheme(myTheme)}>
-      <Box>
-        <img
-          style={{
-            opacity: "0.8",
-            position: "fixed",
-            top: "0",
-            width: "1500px",
-            height: "700px",
-            zIndex: "-1",
-          }}
-          src="https://images.unsplash.com/photo-1563861141459-8d525c27a88e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-          alt="Shibuya Crossing"
-        />
-      </Box>
-      <AppBar position="static">
+      <img
+        style={{
+          opacity: "0.8",
+          position: "fixed",
+          top: "0",
+          width: "100vw",
+          height: "100vh",
+          zIndex: "-1",
+        }}
+        src="https://images.unsplash.com/photo-1563861141459-8d525c27a88e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        alt="Shibuya Crossing"
+      />
+      <AppBar position="static" elevation={0}>
         <Toolbar variant="dense" style={{ backgroundColor: "#FFFFFF" }}>
           <Typography
             style={{
@@ -135,88 +133,72 @@ export default () => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="sm">
-        <Box
-          style={{
-            marginTop: "100px",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            margin: "32px",
-          }}
-        >
-          <Avatar
-            style={{ width: "200px", height: "200px" }}
-            src="https://media-exp1.licdn.com/dms/image/C5603AQF_wD7eIHmZog/profile-displayphoto-shrink_200_200/0?e=1597881600&v=beta&t=mwNm37Qxga4rCR90uI-r8YOMmt0lb_JqzPwW_rJJPGI"
-          />
-          <Typography
+      <Box style={{ display: "flex", height: "auto", flex: "1 0 auto" }}>
+        <Container maxWidth="sm">
+          <Box
+            style={{
+              marginTop: "100px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              margin: "32px",
+            }}
+          >
+            <Avatar
+              style={{ width: "200px", height: "200px" }}
+              src="https://media-exp1.licdn.com/dms/image/C5603AQF_wD7eIHmZog/profile-displayphoto-shrink_200_200/0?e=1597881600&v=beta&t=mwNm37Qxga4rCR90uI-r8YOMmt0lb_JqzPwW_rJJPGI"
+            />
+            <Typography
+              style={{
+                fontFamily: "Roboto",
+                fontWeight: "fontWeightLight",
+                fontSize: "50px",
+                marginTop: "20px",
+                color: "#fff",
+              }}
+              variant="h4"
+            >
+              Hajra Shahab
+            </Typography>
+          </Box>
+
+          <Box
             style={{
               fontFamily: "Roboto",
               fontWeight: "fontWeightLight",
-              fontSize: "50px",
-              marginTop: "20px",
-              color: "#fff",
-            }}
-            variant="h4"
-          >
-            Hajra Shahab
-          </Typography>
-        </Box>
-        <Box
-          style={{
-            height: "200px",
-            width: "500px",
-            backgroundColor: "#FFFFFF",
-            marginRight: "20px",
-            opacity: "0.5",
-          }}
-        >
-          <div
-            name="viewport"
-            content="width= deviceWidth"
-            initialScale="1"
-          ></div>
-        </Box>
-        <Box
-          style={{
-            fontFamily: "Roboto",
-            fontWeight: "fontWeightLight",
-            maxWidth: "700px",
-            height: "100px",
-            marginTop: "-200px",
-            alignItems: "center",
-            textAlign: "center",
-            color: "#fff",
-          }}
-        >
-          <Typography variant="body">
-            Hi, I’m Hajra Shahab, a multidisciplinary researcher with profound
-            interest in data analytics, urban informatics policy, and design. I
-            am a strong advocate of interdiscplinary collaborative approach to
-            unlock unconventional solutions to some of the most pressing urban
-            issues that cities face in the 21st century.
-          </Typography>
-        </Box>
-        <Box>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora",
-              fontWeight: "400px",
-              marginRight: "20px",
-              color: "#fff",
+              alignItems: "center",
               textAlign: "center",
-              marginTop: "20px",
+              padding: "10px",
+              color: "#fff",
+              background: "rgb(255, 255, 255, 0.5)",
             }}
-            variant="h6"
           >
-            Data Analytics | Public Policy | Cities | Transportation and
-            Mobility
-          </Typography>
-        </Box>
-      </Container>
+            <Typography variant="body">
+              Hi, I’m Hajra Shahab, a multidisciplinary researcher with profound
+              interest in data analytics, urban informatics policy, and design.
+              I am a strong advocate of interdiscplinary collaborative approach
+              to unlock unconventional solutions to some of the most pressing
+              urban issues that cities face in the 21st century.
+            </Typography>
+
+            <Typography
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora",
+                fontWeight: "400px",
+                textAlign: "center",
+                marginTop: "20px",
+              }}
+              variant="h6"
+            >
+              Data Analytics | Public Policy | Cities | Transportation and
+              Mobility
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
     </ThemeProvider>
   )
 }
