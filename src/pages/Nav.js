24fd49256fc573ee309744ea5ae45f1b36a9cf7e
Hajra Shahab <hajrashahab05@gmail.com>
2020-06-21 22:26:56 +0500

    Navigation setup

---
+++ src/pages/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function Nav{} {
+    return (
+        <nav>
+            <h3>logo</h3>
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav;
