b2279f0db019e937e0409de5e6b90291937f9ffb
Hajra Shahab <hajrashahab05@gmail.com>
2020-07-12 23:45:25 +0500

    About page links

--- src/pages/About.js
+++ src/pages/About.js
@@ -26,102 +26,128 @@ export default () => {
   // this is where all the code of the main page goes
   return (
     <ThemeProvider theme={createMuiTheme(myTheme)}>
+      <Box // not sure if this is the best way to add background color to the entire page
+        style={{
+          backgroundColor: "#121212",
+          opacity: "1",
+          position: "fixed",
+          top: "0",
+          width: "auto",
+          minWidth: "100vw",
+          height: "140vh",
+          zIndex: "-1",
+        }}
+      ></Box>
       <AppBar
         position="static"
         elevation={0}
         style={{ backgroundColor: "transparent" }}
       >
         <Toolbar variant="dense" style={{ backgroundColor: "transparent" }}>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              marginRight: "20px",
-              color: "#FFB300",
-            }}
-            variant="h5"
-          >
-            EN
+          <Typography variant="h5">
+            <Link
+              to="/"
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora, serif",
+                marginRight: "20px",
+                color: "#FFB300",
+              }}
+            >
+              EN
+            </Link>
           </Typography>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              marginRight: "auto",
-              color: "#FFB300",
-            }}
-            variant="h5"
-          >
-            JP
+          <Typography variant="h5">
+            <Link
+              to="/"
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora, serif",
+                marginRight: "auto",
+                color: "#FFB300",
+              }}
+            >
+              JP
+            </Link>
           </Typography>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              marginLeft: "30px",
-              color: "#FFB300",
-            }}
-            variant="h5"
-          >
-            About
+          <Typography variant="h5">
+            <Link
+              to="/"
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora, serif",
+                marginLeft: "30px",
+                color: "#FFB300",
+              }}
+            >
+              About
+            </Link>
           </Typography>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              marginLeft: "30px",
-              color: "#FFB300",
-            }}
-            variant="h5"
-          >
-            Projects
+          <Typography variant="h5">
+            <Link
+              to="/"
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora, serif",
+                marginLeft: "30px",
+                color: "#FFB300",
+              }}
+            >
+              Projects
+            </Link>
           </Typography>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              marginLeft: "30px",
-              color: "#FFB300",
-            }}
-            variant="h5"
-          >
-            Blog
+          <Typography variant="h5">
+            <Link
+              to="/"
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora, serif",
+                marginLeft: "30px",
+                color: "#FFB300",
+              }}
+            >
+              Blog
+            </Link>
           </Typography>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              marginLeft: "30px",
-              color: "#FFB300",
-            }}
-            variant="h5"
-          >
-            Vlog
+          <Typography variant="h5">
+            <Link
+              to="/"
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora, serif",
+                marginLeft: "30px",
+                color: "#FFB300",
+              }}
+            >
+              Vlog
+            </Link>
           </Typography>
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              marginLeft: "30px",
-              color: "#FFB300",
-            }}
-            variant="h5"
-          >
-            Contact
+          <Typography variant="h5">
+            <Link
+              to="/"
+              style={{
+                href:
+                  "https://fonts.googleapis.com/css2?family=Lora&display=swap",
+                rel: "stylesheet",
+                fontFamily: "Lora, serif",
+                marginLeft: "30px",
+                color: "#FFB300",
+              }}
+            >
+              Contact
+            </Link>
           </Typography>
         </Toolbar>
       </AppBar>
@@ -133,13 +159,19 @@ export default () => {
             fontSize: "50px",
             textAlign: "center",
             marginTop: "20px",
-            color: "#323232",
+            color: "#FFFFFF",
           }}
           variant="h4"
         >
           Meet Hajra!
         </Typography>
-        <Box style={{ background: "rgb(51, 51, 51)", padding: "50px" }}>
+        <Box
+          style={{
+            background: "rgb(51, 51, 51)",
+            padding: "10px",
+            marginTop: "20px",
+          }}
+        >
           <Typography
             style={{
               fontFamily: "Roboto, sans-serif",
@@ -148,9 +180,10 @@ export default () => {
               textAlign: "left",
               marginTop: "10px",
               color: "#FFFFFF",
-              padding: "10px",
+              padding: "20px",
+              verticalAlign: "top",
             }}
-            variant="body"
+            variant="h6"
           >
             ROLE
           </Typography>
@@ -158,13 +191,28 @@ export default () => {
             style={{
               fontFamily: "Roboto, sans-serif",
               fontWeight: "100px",
-              alignItems: "right",
-              textAlign: "right",
-              marginTop: "5px",
+              alignItems: "left",
+              textAlign: "left",
+              marginTop: "10px",
               color: "#FFFFFF",
-              padding: "10px",
+              padding: "20px",
             }}
             variant="body"
+          >
+            Women in Transportation Fellow, 2020-2022
+          </Typography>
+          <Typography
+            style={{
+              fontFamily: "Roboto, sans-serif",
+              fontWeight: "100px",
+              alignItems: "right",
+              textAlign: "right",
+              marginTop: "10px",
+              color: "#FFFFFF",
+              padding: "20px",
+              verticalAlign: "top",
+            }}
+            variant="h6"
           >
             WORK
           </Typography>
@@ -180,70 +228,38 @@ export default () => {
             }}
             variant="body"
           >
+            Its hands were holograms that altered to match the convolutions of
+            the Flatline as a construct.
+          </Typography>
+          <Typography
+            style={{
+              fontFamily: "Roboto, sans-serif",
+              fontWeight: "100px",
+              alignItems: "right",
+              textAlign: "right",
+              marginTop: "5px",
+              color: "#FFFFFF",
+              padding: "10px",
+            }}
+            variant="h6"
+          >
             DATE
           </Typography>
-        </Box>
-        <Box
-          style={{
-            fontFamily: "Lora, serif",
-            fontWeight: "100px",
-            alignItems: "center",
-            textAlign: "center",
-            marginTop: "40px",
-            padding: "40px",
-            color: "#FFFFFF",
-            background: "rgb(51, 51, 51)",
-          }}
-        >
-          <Typography variant="body">Role</Typography>
-
           <Typography
             style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              fontWeight: "400px",
-              textAlign: "center",
-              marginTop: "20px",
+              fontFamily: "Roboto, sans-serif",
+              fontWeight: "100px",
+              alignItems: "left",
+              textAlign: "left",
+              marginTop: "10px",
               color: "#FFFFFF",
+              padding: "10px",
             }}
-            variant="h6"
+            variant="body"
           >
-            Karachi | Lahore | Tokyo | Pittsburgh
+            2020-2022
           </Typography>
         </Box>
-        <Box
-          style={{
-            fontFamily: "Lora, serif",
-            fontWeight: "100px",
-            alignItems: "center",
-            textAlign: "center",
-            marginTop: "40px",
-            padding: "40px",
-            color: "#FFFFFF",
-            background: "rgb(51, 51, 51)",
-          }}
-        >
-          <Typography variant="body">Role</Typography>
-
-          <Typography
-            style={{
-              href:
-                "https://fonts.googleapis.com/css2?family=Lora&display=swap",
-              rel: "stylesheet",
-              fontFamily: "Lora, serif",
-              fontWeight: "400px",
-              textAlign: "center",
-              marginTop: "20px",
-              color: "#FFFFFF",
-            }}
-            variant="h6"
-          >
-            Karachi | Lahore | Tokyo | Pittsburgh
-          </Typography>
-        </Box>
-        <Link to="/">Go back to the homepage</Link>
       </Box>
     </ThemeProvider>
   )
