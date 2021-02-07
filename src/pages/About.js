c9f2458a5921496527ea6162e1762c354d1ea5dc
shayanalis <shayan.ali.work@gmail.com>
2021-02-08 00:07:26 +0500

    remove all other theme providers

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,24 +8,13 @@ import {
   Typography,
   Container,
 } from "@material-ui/core"
-import { ThemeProvider } from "@material-ui/styles"
+
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 export default () => {
-  const myTheme = {
-    palette: {
-      type: "dark",
-      primary: {
-        main: "#343434", // update color
-      },
-      secondary: {
-        main: "#4F8CB3",
-      },
-    },
-  }
   // this is where all the code of the main page goes
   return (
-    <ThemeProvider theme={createMuiTheme(myTheme)}>
+    <>
       <Box // not sure if this is the best way to add background color to the entire page
         style={{
           backgroundColor: "#121212",
@@ -383,6 +372,6 @@ export default () => {
           </Typography>
         </Box>
       </Box>
-    </ThemeProvider>
+    </>
   )
 }
